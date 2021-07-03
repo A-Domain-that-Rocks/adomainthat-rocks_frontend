563b28f3fa4b484bfd101f3fce58857a3a06f953
formidablae <81068781+formidablae@users.noreply.github.com>
2021-07-03 11:33:03 +0200

    Adding SearchForm component

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+const SearchForm = (props: any) => {
+    const onSubmitHandler = (event: any) => {
+        event.preventDefault();
+        // TODO Implement search logic
+        props.onSearchHandler();
+    };
+
+    return (
+        <Form onSubmit={onSubmitHandler}>
+            <Form.Group className="mb-3" controlId="search">
+                <Form.Label>Search</Form.Label>
+                <Form.Control type="text" placeholder="Search" />
+                <Form.Text className="text-muted">
+                </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Search
+            </Button>
+        </Form>
+    );
+};
+
+export default SearchForm;
