13e7367510b3eaffb8c9c6bc3efbc67904318078
formidablae <81068781+formidablae@users.noreply.github.com>
2021-07-04 11:34:13 +0200

    Creating Home layout bootstrap and adding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const onSearchHandler = () => {
+        // Implement the logic for search
+    };
+    return (
+        <div className="App">
+            <Fragment>
+                <Header/>
+                <Container>
+                    <Row>
+                        <Col xs={12} md={4}>
+                            <SearchForm onSearchHandler={onSearchHandler}/>
+                        </Col>
+                        <Col xs={12} md={8}>col 2 of row 1</Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        </div>
+    );
+};
 
 export default App;
