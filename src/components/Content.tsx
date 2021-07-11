346f203e460f3c4d63c4617c00d364cf5e5db698
formidablae <81068781+formidablae@users.noreply.github.com>
2021-07-11 06:20:45 +0200

    Adding functionality to query GraphQL API with Apollo

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import SearchForm from './SearchForm';
+import dotenv from 'dotenv';
+import {
+    ApolloClient
+    , InMemoryCache
+    //, ApolloProvider
+    //, useQuery
+    , gql
+} from '@apollo/client';
+
+dotenv.config();
+
+const myApolloClient = new ApolloClient({
+    uri: `${process.env.REACT_APP_API_URL}`,
+    cache: new InMemoryCache()
+});
 
 const Content = () => {
-    const onSearchHandler = () => {
+    const onSearchHandler = (mySearchValue: String) => {
         // Implement the logic for search
+        const MY_QUERY = gql`
+            query {
+                author(name: "${mySearchValue}") {
+                    name,
+                    orcid,
+                    other_names
+                }
+            }
+        `;
+        myApolloClient.query({ query: MY_QUERY })
+                      .then(result => console.log(result));
     };
+
     return (
         <Container>
             <Row>
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 const SearchForm = (props: any) => {
+    const [searchValue, setSearchValue] = useState('');
+
     const onSubmitHandler = (event: any) => {
         event.preventDefault();
         // TODO Implement search logic
-        props.onSearchHandler();
+        props.onSearchHandler(searchValue);
+    };
+
+    const updateSearchValue = (event: any) => {
+        event.preventDefault();
+        setSearchValue(event.target.value);
     };
 
     return (
         <Form onSubmit={onSubmitHandler}>
             <Form.Group className="mb-3" controlId="search">
                 <Form.Label>Search</Form.Label>
-                <Form.Control type="text" placeholder="Search by author" />
+                <Form.Control type="text"
+                              placeholder="Search by author"
+                              value={searchValue}
+                              onChange={updateSearchValue} />
                 <Form.Text className="text-muted">
                 </Form.Text>
             </Form.Group>
