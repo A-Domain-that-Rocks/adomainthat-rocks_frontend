ed0597fc2bc122ac59ca596ed92ecee8ada155e8
formidablae <81068781+formidablae@users.noreply.github.com>
2021-08-01 12:38:24 +0200

    Testing cytoscape graph with cola or fcose layouts

--- src/components/CytoscapeGraph.tsx
+++ src/components/CytoscapeGraph.tsx
@@ -2,79 +2,87 @@ import Cytoscape from 'cytoscape';
 import CytoscapeComponent from 'react-cytoscapejs';
 // @ts-ignore
 import COSEBilkent from 'cytoscape-cose-bilkent';
+// @ts-ignore
+import Cola from "cytoscape-cola";
+// @ts-ignore
+import fcose from 'cytoscape-fcose';
+
 Cytoscape.use(COSEBilkent);
+Cytoscape.use(Cola);
+Cytoscape.use( fcose );
 
 const CytoscapeGraph = (props: any) => {
     const APIresponseStartNode = props.graphApiResponse.data.nodeGraph.startNode;
     const APIresponseNodes = props.graphApiResponse.data.nodeGraph.vertices;
     const APIresponseEdges = props.graphApiResponse.data.nodeGraph.edges;
 
-    let base_height = 3;
-    let base_width = 3;
-    let base_font_size = 7;
+    //let base_height = 20;
+    //let base_width = 20;
+    //let base_font_size = 7;
     let the_selectable = true;
     let the_locked = false;
     let the_grabbable = true;
-    let base_border_width = 0;
-    let the_border_style = 'solid';
+    //let base_border_width = 0;
+    //let the_border_style = 'solid';
 
     const mappedNodes = APIresponseNodes.map((n: { _id: any; graph_name: any; }, index: any) => {
         let the_type = n._id.split('/')[0];
         let the_name = n.graph_name.substring(0, 20);
-        let the_width = 0;
-        let the_height = 0;
+        //let the_width = 0;
+        //let the_height = 0;
         let the_shape = '';
         let the_background_color = '';
-        let the_font_size = 0;
-        let the_border_width = 0 + base_border_width;
+        //let the_font_size = 0;
+        //let the_border_width = 0 + base_border_width;
+        let the_parent = the_type;
 
         switch(the_type) {
             case ('affiliation_institution'):
-                the_width = 0.75 * base_width;
-                the_height = 0.75 * base_height;
-                the_font_size = 0.8 * base_font_size;
+                //the_width = 0.75 * base_width;
+                //the_height = 0.75 * base_height;
+                //the_font_size = 0.8 * base_font_size;
                 the_shape = 'round-heptagon';
                 the_background_color = 'yellow';
                 break;
             case ('author'):
-                the_width = 1 * base_width;
-                the_height = 1 * base_height;
-                the_font_size = 1 * base_font_size;
+                //the_width = 1 * base_width;
+                //the_height = 1 * base_height;
+                //the_font_size = 1 * base_font_size;
                 the_shape = 'circle';
                 the_background_color = 'green';
                 break;
             case ('editor'):
-                the_width = 0.5 * base_width;
-                the_height = 0.5 * base_height;
-                the_font_size = 0.8 * base_font_size;
+                //the_width = 0.5 * base_width;
+                //the_height = 0.5 * base_height;
+                //the_font_size = 0.8 * base_font_size;
                 the_shape = 'round-pentagon';
                 the_background_color = 'gray';
                 break;
             case ('journal'):
-                the_width = 0.5 * base_width;
-                the_height = 0.5 * base_height;
-                the_font_size = 0.8 * base_font_size;
+                //the_width = 0.5 * base_width;
+                //the_height = 0.5 * base_height;
+                //the_font_size = 0.8 * base_font_size;
                 the_shape = 'round-rectangle';
                 the_background_color = 'pink';
                 break;
             case ('publication'):
-                the_width = 0.75 * base_width;
-                the_height = 0.75 * base_height;
-                the_font_size = 0.6 * base_font_size;
+                //the_width = 0.75 * base_width;
+                //the_height = 0.75 * base_height;
+                //the_font_size = 0.6 * base_font_size;
                 the_shape = 'octagon';
                 the_background_color = 'blue';
                 break;
             case ('school'):
-                the_width = 0.67 * base_width;
-                the_height = 0.67 * base_height;
-                the_font_size = 0.6 * base_font_size;
+                //the_width = 0.67 * base_width;
+                //the_height = 0.67 * base_height;
+                //the_font_size = 0.6 * base_font_size;
                 the_shape = 'round-hexagon';
                 the_background_color = 'red';
                 break;
             case ('series'):
-                the_width = 0.5 * base_width;
-                the_height = 0.5 * base_height;
-                the_font_size = 0.6 * base_font_size;
+                //the_width = 0.5 * base_width;
+                //the_height = 0.5 * base_height;
+                //the_font_size = 0.6 * base_font_size;
                 the_shape = 'round-diamond';
                 the_background_color = 'purple';
                 break;
@@ -83,12 +91,12 @@ const CytoscapeGraph = (props: any) => {
         }
 
         if (n._id === APIresponseStartNode._id) {
-            the_width = 2 * base_width;
-            the_height = 2 * base_height;
+            //the_width = 2 * base_width;
+            //the_height = 2 * base_height;
             //the_background_color = 'gray';
-            the_font_size = 1 * base_font_size;
-            the_border_width = 0.5 + base_border_width;
-            the_border_style = 'dotted';
+            //the_font_size = 1 * base_font_size;
+            //the_border_width = 0.5 + base_border_width;
+            //the_border_style = 'dotted';
         }
 
         let constructed_node = {
@@ -97,13 +105,13 @@ const CytoscapeGraph = (props: any) => {
                 'label': the_name
             },
             style: {
-                width: the_width,
-                height: the_height,
+                //width: the_width,
+                //height: the_height,
                 'background-color': the_background_color,
                 shape: the_shape,
-                'font-size': the_font_size,
-                'border-width': 0 + the_border_width,
-                'border-style': the_border_style
+                //'font-size': the_font_size,
+                //'border-width': 0 + the_border_width,
+                //'border-style': the_border_style
             },
             //position: { x: Math.floor(Math.random() * 1000),
             //            y: Math.floor(Math.random() * 1000) },
@@ -132,7 +140,7 @@ const CytoscapeGraph = (props: any) => {
 
     const graphElements = [ ...mappedNodes, ...mappedEdges];
 
-    let layout_options = {
+    let coseLayoutOptions = {
         name: 'cose',
       
         // Called on `layoutready`
@@ -254,7 +262,7 @@ const CytoscapeGraph = (props: any) => {
         gravityRangeCompound: 1.5,
 
         // Ideal (intra-graph) edge length
-        idealEdgeLength: 50,
+        //idealEdgeLength: Math.min(base_height, base_width) * 10,
 
         // Initial cooling factor for incremental layout
         initialEnergyOnIncremental: 0.5,
@@ -299,6 +307,110 @@ const CytoscapeGraph = (props: any) => {
 
     };
 
+    let colaSpringyLayoutOptions = {
+        name: 'cola',
+      
+        // Called on `layoutready`
+        ready: function(){},
+      
+        // Called on `layoutstop`
+        stop: function(){},
+
+        fit: true, // whether to fit the viewport to the graph
+        padding: 10, // the padding on fit
+        animate: true,
+        edgeLength: 300, // sets edge length directly in simulation
+        nodeSpacing: 50
+    };
+
+    let fcodeLayoutOptions = {
+
+        name: 'fcose',
+
+        // 'draft', 'default' or 'proof' 
+        // - "draft" only applies spectral layout 
+        // - "default" improves the quality with incremental layout (fast cooling rate)
+        // - "proof" improves the quality with incremental layout (slow cooling rate) 
+        quality: "default",
+        // Use random node positions at beginning of layout
+        // if this is set to false, then quality option must be "proof"
+        randomize: true, 
+        // Whether or not to animate the layout
+        animate: true, 
+        // Duration of animation in ms, if enabled
+        animationDuration: 1000, 
+        // Easing of animation, if enabled
+        animationEasing: undefined, 
+        // Fit the viewport to the repositioned nodes
+        fit: true, 
+        // Padding around layout
+        padding: 30,
+        // Whether to include labels in node dimensions. Valid in "proof" quality
+        nodeDimensionsIncludeLabels: false,
+        // Whether or not simple nodes (non-compound nodes) are of uniform dimensions
+        uniformNodeDimensions: false,
+        // Whether to pack disconnected components - cytoscape-layout-utilities extension should be registered and initialized
+        packComponents: true,
+        // Layout step - all, transformed, enforced, cose - for debug purpose only
+        step: "all",
+        
+        /* spectral layout options */
+        
+        // False for random, true for greedy sampling
+        samplingType: true,
+        // Sample size to construct distance matrix
+        sampleSize: 25,
+        // Separation amount between nodes
+        nodeSeparation: 75,
+        // Power iteration tolerance
+        piTol: 0.0000001,
+        
+        /* incremental layout options */
+        
+        // Node repulsion (non overlapping) multiplier
+        nodeRepulsion: function( node: any ){ return 4500; },
+        // Ideal edge (non nested) length
+        idealEdgeLength: function( edge: any ){ return 50; },
+        // Divisor to compute edge forces
+        edgeElasticity: function( edge: any ){ return 0.45; },
+        // Nesting factor (multiplier) to compute ideal edge length for nested edges
+        nestingFactor: 0.1,
+        // Maximum number of iterations to perform - this is a suggested value and might be adjusted by the algorithm as required
+        numIter: 2500,
+        // For enabling tiling
+        tile: true,  
+        // Represents the amount of the vertical space to put between the zero degree members during the tiling operation(can also be a function)
+        tilingPaddingVertical: 10,
+        // Represents the amount of the horizontal space to put between the zero degree members during the tiling operation(can also be a function)
+        tilingPaddingHorizontal: 10,
+        // Gravity force (constant)
+        gravity: 0.25,
+        // Gravity range (constant) for compounds
+        gravityRangeCompound: 1.5,
+        // Gravity force (constant) for compounds
+        gravityCompound: 1.0,
+        // Gravity range (constant)
+        gravityRange: 3.8, 
+        // Initial cooling factor for incremental layout  
+        initialEnergyOnIncremental: 0.3,
+      
+        /* constraint options */
+      
+        // Fix desired nodes to predefined positions
+        // [{nodeId: 'n1', position: {x: 100, y: 200}}, {...}]
+        fixedNodeConstraint: undefined,
+        // Align desired nodes in vertical/horizontal direction
+        // {vertical: [['n1', 'n2'], [...]], horizontal: [['n2', 'n4'], [...]]}
+        alignmentConstraint: undefined,
+        // Place two nodes relatively in vertical/horizontal direction
+        // [{top: 'n1', bottom: 'n2', gap: 100}, {left: 'n3', right: 'n4', gap: 75}, {...}]
+        relativePlacementConstraint: undefined,
+      
+        /* layout event callbacks */
+        ready: () => {}, // on layoutready
+        stop: () => {} // on layoutstop
+    };
+
     return (<CytoscapeComponent
             elements={graphElements}
             style={{
@@ -310,7 +422,8 @@ const CytoscapeGraph = (props: any) => {
                 border: 1
             }}
             // style={ { width: '500px', height: '75vh' } }
-            layout={coseBilkentLayoutOptions}
+            // @ts-ignore
+            layout={colaSpringyLayoutOptions}
         />)
 };
 
