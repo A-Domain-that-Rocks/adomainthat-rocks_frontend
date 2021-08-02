6bbeed0ce6fc29ffd13e20547d38c5c08fdb6f00
formidablae <81068781+formidablae@users.noreply.github.com>
2021-08-02 13:24:05 +0200

    Made search form validation rules, states and styles with bootstrap

--- src/components/SearchFormNodeGraph.tsx
+++ src/components/SearchFormNodeGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Form, Button, ListGroup } from 'react-bootstrap';
 import { myApolloClient } from '../App';
 import { gql } from '@apollo/client';
@@ -10,19 +10,87 @@ const SearchFormNodeGraph = (props: any) => {
     const [isQueryingAPI, setIsQueryingAPI] = useState(new Map<string, boolean>());
     const [resultsObtained, setResultsObtained] = useState(new Set());
     const [searchValueNodeName, setSearchValueNodeName] = useState('');
-    const [searchValueNodeID, setSearchValueNodeID] = useState('');
-    const [minDepthValue, setMinDepthValue] = useState(1);
-    const [maxDepthValue, setMaxDepthValue] = useState(2);
+    const [searchValueNodeID, setSearchValueNodeID] = useState(''); // to do refactor with formInputs.input.nodeId
+    const [minDepthValue, setMinDepthValue] = useState(1); // to do refactor with formInputs.input.min
+    const [maxDepthValue, setMaxDepthValue] = useState(2); // to do refactor with formInputs.input.max
     const [suggestions, setSuggestions] = useState([]);
     const [lastChangeTimeoutTimerId, setLastChangeTimeoutTimerId] = useState(0);
+    const [formInputs, setFormInputs] = useState({
+        input: {nodeId: '', min: 1, max: 2}, // to do refactor with searchValueNodeID, minDepthValue and maxDepthValue
+        errors: {nodeId: '', min: '', max: ''}
+    });
+    const [isValidatedAtLeastOnce, setIsValidatedAtLeastOnce] = useState(false);
 
     const onSubmitHandler = (event: any) => {
         event.preventDefault();
-        if (searchValueNodeID === '' || minDepthValue === null || maxDepthValue === null) {
-            //window.alert(`At least one of the search fields is empty... Please fill in the fields.`);
-        } else {
+        setIsValidatedAtLeastOnce(true)
+        if (validate()) {
+            setFormInputs({input: {nodeId: "", min: 1, max: 2}, errors: {nodeId: '', min: '', max: ''}});
             props.onSearchHandler(searchValueNodeID, minDepthValue, maxDepthValue);
         }
+    };
+
+    useEffect( () => {
+            if (isValidatedAtLeastOnce) { validate(); }
+        }, [searchValueNodeID, minDepthValue, maxDepthValue]
+    );
+
+    const validate = () => {
+        let input = formInputs.input;
+        let errors = {
+            min: '',
+            max: '',
+            nodeId: ''
+        };
+        let isValid = true;
+
+        //Name is required - check if it is empty
+        if (!input["nodeId"] || input["nodeId"] === '') {
+            isValid = false;
+            errors["nodeId"] = "Choose a node name from the list";
+        }
+
+        // Minimum depth validation checks
+        if (!input["min"]) {
+            isValid = false;
+            errors["min"] = "Minimum depth is required";
+        }
+        if ((input["min"] < 1 && !input["max"]) || (input["min"] < 1 && input["min"] <= input["max"])) {
+            isValid = false;
+            errors["min"] = "Minimum depth has to be greater or equal to 1";
+        }
+        if (input["min"] < 1 && input["min"] > input["max"]) {
+            isValid = false;
+            errors["min"] = "Minimum depth has to be greater or equal to maximum depth and it has to be greater or equal to 1";
+        }
+        if (input["min"] > 1 && input["min"] > input["max"]) {
+            isValid = false;
+            errors["min"] = "Minimum depth has to be smaller or equal to maximum depth";
+        }
+
+        //Maximum depth checks
+        if (!input["max"]) {
+            isValid = false;
+            errors["max"] = "Maximum depth is required";
+        }
+        if (input["max"] >= 1 && input["max"] < input["min"]) {
+            isValid = false;
+            errors["max"] = "Maximum depth has to be greater or equal to minimum depth";
+        }
+        if (input["max"] < 1 && input["max"] >= input["min"]) {
+            isValid = false;
+            errors["max"] = "Maximum depth has to be greater or equal to 1";
+        }
+        if (input["max"] < 1 && input["max"] < input["min"]) {
+            isValid = false;
+            errors["max"] = "Maximum depth has to be greater or equal to minimum depth and it has to be greater or equal to 1";
+        }
+
+        setFormInputs((prevState) => ({
+            ...prevState,
+            errors
+        }));
+        return isValid;
     };
 
     const searchQueryBuilder = (searchV: string, num: string) => {
@@ -37,11 +105,11 @@ const SearchFormNodeGraph = (props: any) => {
                 }
             }
         `;
-    }
+    };
 
     const queryAPIforSuggestions = async (searchVal: string) => {
         // const lowerSearchVal = searchVal.toLowerCase()
-        // 
+        //
         // isQueryingAPI.set(lowerSearchVal, true);
         // setIsQueryingAPI(isQueryingAPI);
         //
@@ -76,11 +144,11 @@ const SearchFormNodeGraph = (props: any) => {
         isQueryingAPI.set(lowerSearchVal, true);
         setIsQueryingAPI(isQueryingAPI);
 
-        const result2: any = await myApolloClient.query({ query: searchQueryBuilder(searchVal, "2") });
-        
+        const result2: any = await myApolloClient.query({query: searchQueryBuilder(searchVal, "2")});
+
         searchValueDict.set(lowerSearchVal, result2.data.nodesID2);
         setSearchValueDict(searchValueDict);
-        
+
         resultsObtained.add(lowerSearchVal);
         setResultsObtained(resultsObtained);
 
@@ -114,9 +182,9 @@ const SearchFormNodeGraph = (props: any) => {
         lastValue = argVal;
 
         if (event.target.value.length > 4 && !resultsObtained.has(argVal.toLowerCase()) && !isQueryingAPI.has(argVal.toLowerCase())) {
-            const newTimeoutTimerId: any = setTimeout ( function() {
+            const newTimeoutTimerId: any = setTimeout(function() {
                 queryAPIforSuggestions(argVal);
-            }, 750 );
+            }, 750);
             setLastChangeTimeoutTimerId(newTimeoutTimerId)
         } else if (event.target.value.length > 4 && resultsObtained.has(argVal.toLowerCase())) {
             const suggest: any = searchValueDict.get(argVal.toLowerCase());
@@ -124,21 +192,40 @@ const SearchFormNodeGraph = (props: any) => {
         }
     };
 
-    const onSuggestionSelectionHandler = (id: any, name: any) => {
-        setSearchValueNodeID(id);
-        setSearchValueNodeName(name);
+    const onSuggestionSelectionHandler = (suggestion: any) => {
+        setSearchValueNodeID(suggestion._id);
+        setFormInputs((prevState) => ({
+            ...prevState,
+            input: {
+                ...prevState.input,
+                nodeId: suggestion._id
+            }
+        }));
+        setSearchValueNodeName(suggestion.graph_name);
         setSuggestions([]);
     };
 
     const updateMinDepthValue = (event: any) => {
         event.preventDefault();
-        // console.log('updateMinDepthValue', event.target.value)
+        setFormInputs((prevState) => ({
+            ...prevState,
+            input: {
+                ...prevState.input,
+                [event.target.name]: parseInt(event.target.value) // to do refactor to outer function call
+            },
+        }));
         setMinDepthValue(event.target.value);
     };
 
     const updateMaxDepthValue = (event: any) => {
         event.preventDefault();
-        // console.log('updateMaxDepthValue', event.target.value)
+        setFormInputs((prevState) => ({
+            ...prevState,
+            input: {
+                ...prevState.input,
+                [event.target.name]: parseInt(event.target.value) // to do refactor to outer function call
+            },
+        }));
         setMaxDepthValue(event.target.value);
     };
 
@@ -150,52 +237,63 @@ const SearchFormNodeGraph = (props: any) => {
 
                 <Form.Text className="text-muted">Node name or title</Form.Text>
                 <Form.Control type="text"
+                              name="nodeId"
                               placeholder="Enter a name or a title"
                               value={searchValueNodeName}
                               onChange={updateSearchValueNodeName}
+                              isInvalid={formInputs.errors.nodeId !== ''}
+                              isValid={isValidatedAtLeastOnce && formInputs.input.nodeId !== ''}
                               onBlur={() => {
                                   setTimeout(() => {
                                       setSuggestions([])
                                   }, 100);
                               }}
-                              onFocus={updateSearchValueNodeName} 
-                              style={ searchValueNodeName === '' || resultsObtained.size === 0 ? {backgroundColor: 'rgb(30, 30, 30)'}: (searchValueNodeID === '' ? {backgroundColor: 'yellow', color: 'rgb(30, 30, 30)'}: {backgroundColor: 'darkgreen'}) }
+                              onFocus={updateSearchValueNodeName}
                 />
+                <Form.Control.Feedback type="invalid">
+                    {formInputs.errors.nodeId}
+                </Form.Control.Feedback>
 
-                {/*Suggestion section*/}
                 <ListGroup className="suggestionList">
-                    {/*{isQueryingAPI.has(searchValueNodeName) && isQueryingAPI.get(searchValueNodeName) ?*/}
-                    {/*    <div>Loading possible nodes ... </div> : searchValueNodeID === "" ? <div></div> :*/}
                     {suggestions && suggestions.length > 0 && suggestions.map((this_suggestion: any, i) =>
-                        <ListGroup.Item key={i}>
-                            <div
-                                onClick={() => onSuggestionSelectionHandler(this_suggestion._id, this_suggestion.graph_name)}>
-                                {this_suggestion.the_type[0].toUpperCase() + this_suggestion.the_type.slice(1) + ": " + this_suggestion.graph_name}
-                            </div>
+                        <ListGroup.Item
+                            key={i}
+                            onClick={() => onSuggestionSelectionHandler(this_suggestion)}
+                        >
+                            {this_suggestion.the_type[0].toUpperCase() + this_suggestion.the_type.slice(1) + ": " + this_suggestion.graph_name}
                         </ListGroup.Item>
                     )}
                 </ListGroup>
-                
+            </Form.Group>
+
+            <Form.Group>
                 <Form.Text className="text-muted">Minimum depth</Form.Text>
                 <Form.Control type="number"
+                              name="min"
                               placeholder="Set minimum depth, e.g: 1"
                               value={minDepthValue}
+                              isInvalid={formInputs.errors.min !== ''}
+                              isValid={isValidatedAtLeastOnce && formInputs.errors.min === ''}
                               onChange={updateMinDepthValue}
-                              style={minDepthValue == 1 || (minDepthValue > 1 && maxDepthValue >= minDepthValue) ? {backgroundColor: 'darkgreen'}: {backgroundColor: 'yellow', color: 'rgb(30, 30, 30)'} }
                 />
-                {minDepthValue < 1 && minDepthValue <= maxDepthValue ? <Form.Text className="text-warning">Minimum depth has to be greater or equal to 1</Form.Text>: <Form.Text className="text-warning"></Form.Text>}
-                {minDepthValue < 1 && minDepthValue > maxDepthValue ? <Form.Text className="text-warning">Minimum depth has to be greater or equal to maximum depth and it has to be greater or equal to 1</Form.Text>: <Form.Text className="text-warning"></Form.Text>}
-                {minDepthValue > 1 && minDepthValue > maxDepthValue ? <Form.Text className="text-warning">Minimum depth has to be smaller or equal to maximum depth</Form.Text>: <Form.Text className="text-warning"></Form.Text>}
+                <Form.Control.Feedback type="invalid">
+                    {formInputs.errors.min}
+                </Form.Control.Feedback>
+            </Form.Group>
+
+            <Form.Group>
                 <Form.Text className="text-muted">Maximum depth</Form.Text>
                 <Form.Control type="number"
+                              name="max"
                               placeholder="Set maximum depth, e.g: 2"
                               value={maxDepthValue}
                               onChange={updateMaxDepthValue}
-                              style={maxDepthValue >= minDepthValue && maxDepthValue >= 1 ? {backgroundColor: 'darkgreen'}: {backgroundColor: 'yellow', color: 'rgb(30, 30, 30)'} }
+                              isInvalid={formInputs.errors.max !== ''}
+                              isValid={isValidatedAtLeastOnce && formInputs.errors.max === ''}
                 />
-                {maxDepthValue >= 1 && maxDepthValue < minDepthValue ? <Form.Text className="text-warning">Maximum depth has to be greater or equal to minimum depth</Form.Text>: <Form.Text className="text-warning"></Form.Text>}
-                {maxDepthValue < 1 && maxDepthValue >= minDepthValue ? <Form.Text className="text-warning">Maximum depth has to be greater or equal to 1</Form.Text>: <Form.Text className="text-warning"></Form.Text>}
-                {maxDepthValue < 1 && maxDepthValue < minDepthValue ? <Form.Text className="text-warning">Maximum depth has to be greater or equal to minimum depth and it has to be greater or equal to 1</Form.Text>: <Form.Text className="text-warning"></Form.Text>}
+                <Form.Control.Feedback type="invalid">
+                    {formInputs.errors.max}
+                </Form.Control.Feedback>
                 <Button className="searchButton" variant="dark" type="submit">Search</Button>
             </Form.Group>
         </Form>
